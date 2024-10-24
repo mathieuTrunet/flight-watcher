8ae82e24aa20eac116d9bb5916ab0c7124f6f89e
mathieuTrunet <mathieu1508@gmail.com>
2024-10-24 14:10:11 +0200

    setup: frontend dependencies and configs

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="vite/client" />
+
+type Flight = {
+  icao24: string
+  callsign?: string | null
+  origin_country: string
+  time_position?: number | null
+  last_contact?: number | null
+  longitude?: number | null
+  latitude?: number | null
+  baro_altitude?: number | null
+  on_ground: boolean
+  velocity?: number | null
+  true_track?: number | null
+  vertical_rate?: number | null
+  sensors?: Array<number> | null
+  geo_altitude?: number | null
+  squawk?: string | null
+  spi: boolean
+  position_source: number
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { preprocess } from 'zod'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: { preprocessorOptions: { scss: { implementation: 'sass' } } },
   server: {
     proxy: {
       '/api': { target: 'http://backend:4000', changeOrigin: true, rewrite: path => path.replace('^/api', '') },
