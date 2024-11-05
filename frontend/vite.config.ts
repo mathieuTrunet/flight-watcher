4f089def7a49bfc14278deabc075fcc83761679e
mathieuTrunet <mathieu1508@gmail.com>
2024-11-05 19:06:21 +0100

    fix: fixed environment for development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,6 @@ export default defineConfig({
   plugins: [vue()],
   css: { preprocessorOptions: { scss: { implementation: 'sass' } } },
   server: {
-    proxy: {
-      '/api': { target: 'http://backend:4000', changeOrigin: true, rewrite: path => path.replace('^/api', '') },
-    },
     host: true,
     port: 3000,
   },
